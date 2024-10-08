898d47f1b40a71cc167b50cf84b59f621eb00fce
MasZam12 <135273176+MasZam12@users.noreply.github.com>
2024-10-08 22:18:04 +0700

    Menghapus UI.py dan melakukan beberapa perubahan

--- Inference_Forward_Chaining.py
+++ Inference_Forward_Chaining.py
@@ -2,27 +2,23 @@
 from Rule import corn_diseases, symptoms, symptoms_name
 
 def detect_disease():
-    detected_diseases = set()  # Menggunakan set untuk menghindari duplikasi
+    detected_diseases = set()
 
-    # Tanyakan setiap penyakit
     for disease_name, disease_code in corn_diseases.items():
         symptom_list = symptoms[disease_code]
-        disease_detected = True  # Asumsi awal penyakit terdeteksi
+        disease_detected = True
 
-        # Tanyakan setiap gejala terkait dengan penyakit
         for symptom in symptom_list:
-            symptom_name = symptoms_name[symptom]  # Mengambil nama gejala
-            response = input(f"Apakah jagung mengalami {symptom_name}? (ya/tidak): ").strip().lower()
+            symptom_name = symptoms_name[symptom]
+            response = input(f"Apakah jagung mengalami gejala {symptom_name} atau {symptom}? (ya/tidak): ").strip().lower()
             if response == 'tidak':
-                disease_detected = False  # Jika ada gejala yang tidak terdeteksi, set flag ke False
-                break  # Hentikan pertanyaan untuk gejala selanjutnya
+                disease_detected = False
+                break
 
-        # Jika semua gejala terdeteksi (semua "ya"), tambahkan kode penyakit ke dalam set
         if disease_detected:
             detected_diseases.add(disease_code)
             print(f"\nPenyakit yang mungkin terjadi: {disease_name} ({disease_code})")
-            break  # Hentikan pertanyaan lebih lanjut setelah penyakit terdeteksi
+            break
 
-    # Jika tidak ada penyakit terdeteksi
     if not detected_diseases:
         print("Tidak ada penyakit yang terdeteksi berdasarkan gejala yang diberikan.")
